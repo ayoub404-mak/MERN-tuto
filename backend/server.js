4a3f10e8b3a1795bbcc223d3d8ff33e738ec6065
ayoub404-mak <ayoub151539@gmail.com>
2026-04-19 19:59:45 +0200

    mongoose setup & test

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // express app
@@ -16,10 +17,21 @@ app.use((req, res, next)=>{
 // routes
 app.use('/api/workouts',workoutRoutes)
 
-//lisent for request
-app.listen(process.env.PORT, () => {
-    console.log('listening on port',process.env.PORT)
-})
+// connect to db 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+
+        //lisent for request
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port',process.env.PORT)
+        })
+
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
 
 
