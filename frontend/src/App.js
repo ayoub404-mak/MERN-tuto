d71fcd23aaa8519a889f6ebf651868b759202b3d
ayoub404-mak <ayoub151539@gmail.com>
2026-04-22 13:32:21 +0200

    add react context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-
-// pages & components 
+// pages & components
 import Home from './pages/Home'
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
 
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <div className="pages">
           <Routes>
-            <Route
-              path="/"
-              element={<Home/>}
+            <Route 
+              path="/" 
+              element={<Home />} 
             />
           </Routes>
         </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,33 +1,36 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
-// components 
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkoutForm from '../components/WorkoutForm'
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
-    useEffect(() => {
-        const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
-            const json = await response.json()
+  const { workouts, dispatch } = useWorkoutsContext()
 
-            if (response.ok){
-                setWorkouts(json)
-            }
-        }
-        fetchWorkouts()
-    }, [])
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
 
-    return (
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout) => (
-                    <WorkoutDetails key={workout._id} workout = {workout}/>
-                ))}
-            </div>
-            <WorkoutForm/>
-        </div>
-    )
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
+
+    fetchWorkouts()
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  )
 }
 
 export default Home
